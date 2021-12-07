1d0db90bb2ff67b192db1bdc48d8128ac2971fff
jiwon0995 <jiwon0995@gmail.com>
2021-12-07 22:01:31 +0900

    Add : BoardList CSS

--- src/components/board/list/BoardList.pregenter.tsx
+++ src/components/board/list/BoardList.pregenter.tsx
@@ -1,4 +1,16 @@
+import styled from "@emotion/styled"
+
+
+const Wrapper = styled.div`
+  
+`
 export default function BoardListUI() { 
 
-  return<></>
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
 }
