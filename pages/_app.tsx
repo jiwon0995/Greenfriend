dcd2ae7d27af4e6c6566fbec11becedc78bd02a4
jiwon0995 <jiwon0995@gmail.com>
2021-12-11 13:14:48 +0900

    Update : app.tsx 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import {
+	ApolloClient,
+	ApolloLink,
+	ApolloProvider,
+	InMemoryCache,
+} from '@apollo/client';
+import 'antd/dist/antd.css';
+import { Global } from '@emotion/react';
+import { createUploadLink } from 'apollo-upload-client';
+import { createContext, useState, useEffect } from 'react';
+import { onError } from '@apollo/client/link/error';
+import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+	
+	const uploadLink = createUploadLink({
+		uri: 'http://backend03.codebootcamp.co.kr/graphql',
+		credentials: 'include', // 중요한 정보들을 포함시켜줘. 이거 해야 쿠키에 저장됨
+	}); // 필요한 부분만 따로 만들어서 조립하는 방식
+
+	const client = new ApolloClient({
+		link: ApolloLink.from([errorLink, uploadLink as unknown as ApolloLink]),
+		cache: new InMemoryCache(),
+	});
+
+	return (
+		<>
+				<ApolloProvider client={client}>
+						<Component {...pageProps} />
+				</ApolloProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
